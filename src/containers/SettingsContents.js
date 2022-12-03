997adafbe2871ba3dc00cbc16a780449966b2ce1
dbatista1122 <71308097+dbatista1122@users.noreply.github.com>
2022-12-02 22:34:01 -0500

    merging with main

--- src/containers/SettingsContents.js
+++ src/containers/SettingsContents.js
@@ -8,43 +8,76 @@ import {
   Button,
   HStack
 } from '@chakra-ui/react'
+import axios from 'axios';
 
 function SettingContents() {  
 
-    const [userinfo, setUserInfo] = useState({
-      categories: [],
-      response: [],
-    });
-    
-    const handleChange = (e) => {
-      const { value, checked } = e.target;
-      const { categories } = userinfo;
-      if (checked) {
-        setUserInfo({
-          categories: [...categories, value],
-          response: [...categories, value],
-        });
-      }
-      else {
-        setUserInfo({
-          categories: categories.filter((e) => e !== value),
-          response: categories.filter((e) => e !== value),
-        });
-      }
-    };
+  const [userinfo, setUserInfo] = useState({
+    categories: [],
+    response: [],
+  });
+  
+  const handleChange = (e) => {
+    const { value, checked } = e.target;
+    const { categories } = userinfo;
+    if (checked) {
+      setUserInfo({
+        categories: [...categories, value],
+        response: [...categories, value],
+      });
+    }
+    else {
+      setUserInfo({
+        categories: categories.filter((e) => e !== value),
+        response: categories.filter((e) => e !== value),
+      });
+    }
+  };
 
   function updatePrefereneces() {
-
-
-
+    var token = localStorage.getItem('token');
+    const body = {
+      radius: 32
+      // opt-out: "true",
+    };
+    axios.put('auth/login/', body)
+    .then((res) => {
+      console.log(res.data.profile.settings);
+    }, (error) => {
+      console.log(error);
+    });
   };
 
   function resetPreferences() {
+    var token = localStorage.getItem('token');
+    const body = {
+      radius: 32
+      // opt-out: "true",
+    };
+    axios.put('auth/login/', body)
+    .then((res) => {
+      console.log(res.data.profile.settings);
+    }, (error) => {
+      console.log(error);
+    });
+  }
+
+  function getPreferences() {
+    var token = localStorage.getItem('token');
+    const body = {
+      id: token
+    }
+    axios.post('auth/login/', body)
+    .then((res) => {
+      console.log(res.data.profile.settings);
+    }, (error) => {
+      console.log(error);
+    });
 
   }
 
   return (
-    <Box as="nav">
+    <Box as="nav" onLoad={getPreferences()}>
       <Grid templateColumns='repeat(2, 1fr)' gap={4} paddingTop='10vh' justify={'center'}> 
         <FormLabel htmlFor='name'>Share my data with organizations.</FormLabel>
         <Box>
@@ -59,25 +92,24 @@ function SettingContents() {
           />
         </Box>
       </Grid>
-
-    <HStack paddingTop={'30vh'} justifyContent='right'> 
-    <Button mr={3} 
-            onClick={ () => {
-              resetPreferences();
-              handleChange();
-            }}
-            colorScheme='green'>
-            Reset
-    </Button>
-    <Button mr={3} 
-            onClick={ () => {
-              updatePrefereneces();
-              handleChange();
-            }}
-            colorScheme='blue'>
-            Apply
+      <HStack paddingTop={'30vh'} justifyContent='right'> 
+      <Button mr={3} 
+              onClick={ () => {
+                resetPreferences();
+                handleChange();
+              }}
+              colorScheme='green'>
+              Reset
       </Button>
-    </HStack>
+      <Button mr={3} 
+              onClick={ () => {
+                updatePrefereneces();
+                handleChange();
+              }}
+              colorScheme='blue'>
+              Apply
+        </Button>
+      </HStack>
    
 
     </Box>
